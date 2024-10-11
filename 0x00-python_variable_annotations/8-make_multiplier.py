368624d25669c551d60223ac34c303ee3d63994d
kan-215 <katamialvin369@gmail.com>
2024-10-12 00:35:52 +0300

    0x00-python_variable_annotations

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,16 +1,10 @@
-
 #!/usr/bin/env python3
-""" Complex types - functions"""
-from typing import Callable, Iterator, Union, Optional, List, Tuple
+"""Basic annotations - make_multiplier"""
+
+from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """
-    function takes a float multiplier as argument,
-    returns a function that multiplies a float by multiplier.
-    """
-    def f(n: float) -> float:
-        return float(n * multiplier)
+    """Function make_multiplier"""
 
-    return f
-
+    return lambda x: x * multiplier
