892a7aee7111ce663994f15af6fe2dc09d9bb9d6
kan-215 <katamialvin369@gmail.com>
2024-10-14 22:47:27 +0300

    0x01-python_async_function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,7 +6,12 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """taking the code from wait_n and altering it into a new fuction task_wait_n"""
+    """
+        Take the code from wait_n and alter it into a
+        new function task_wait_n. The code is nearly
+        identical to wait_n except task_wait_random
+        is being called.
+    """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
 
